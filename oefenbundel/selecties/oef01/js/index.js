a13bdf334131a7aa91e60959a40e46a8f4bedb27
Your Name <you@example.com>
2023-09-12 15:42:43 +0200

    title toegevoegd bij oef2

---
+++ oefenbundel/selecties/oef01/js/index.js
@@ -0,0 +1,6 @@
+let leeftijd = prompt("Voeg in uw leeftijd.");
+if (leeftijd>=18){
+    console.log("Je bent " + leeftijd + " jaar oud. Je mag deelnemen aan de spelen van de Nationale Loterij.");
+}else{
+    console.log("Om deel te nemen aan de spelen van de Nationale Loterij moet je minimum 18 jaar oud zijn.");
+}
